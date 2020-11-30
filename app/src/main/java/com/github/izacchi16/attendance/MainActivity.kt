2288e82691acb623a1e96dac0261a49cb1057660
Izacchi <sakuya1202@icloud.com>
2020-11-30 11:42:01 +0900

    init

---
+++ app/src/main/java/com/github/izacchi16/attendance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.izacchi16.attendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
